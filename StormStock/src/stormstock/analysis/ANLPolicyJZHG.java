b6fa9b8060bf3af47ac4ff9ce9cd4b9716340508
wudi <wudi19870903@163.com>
2016-09-19 17:55:19 +0800

    x

--- StormStock/src/stormstock/analysis/ANLPolicyJZHG.java
+++ StormStock/src/stormstock/analysis/ANLPolicyJZHG.java
@@ -52,7 +52,7 @@ public class ANLPolicyJZHG extends ANLPolicy {
 		}
 		// PE市盈率过大的过滤掉
 		if(cANLStock.curBaseInfo.peRatio > 100.0f 
-				&& cANLStock.curBaseInfo.peRatio > 0)
+				|| cANLStock.curBaseInfo.peRatio == 0)
 		{
 			return false;
 		}
@@ -64,10 +64,14 @@ public class ANLPolicyJZHG extends ANLPolicy {
 			return false;
 		}
 		
-//		if(cANLStock.id.compareTo("300312") == 0 || cANLStock.id.compareTo("300314") == 0)
+//		if(cANLStock.id.compareTo("300312") == 0 )
 //		{
 //			return true;
 //		}
+//		else{
+//			return false;
+//		}
+		
 		String logstr = String.format("add userpool %s %s\n", cANLStock.id, cANLStock.curBaseInfo.name);
 		outputLog(logstr);
 		return true;
@@ -92,22 +96,43 @@ public class ANLPolicyJZHG extends ANLPolicy {
 			// 当前价格参数
 			float cur_price = cANLStock.historyData.get(cANLStock.historyData.size()-1).close;
 			float cur_pricePa = (cur_price - long_base_price)/long_base_price;
-			// 中期最高价格参数
-			float mid_high_price = cANLStock.GetHigh(120, date);
-			float mid_high_pricePa = (mid_high_price - long_base_price)/long_base_price;
-			// 中期最低价格参数
-			float mid_low_price = cANLStock.GetLow(120, date);
+			
+			// 计算短期偏离比
+			float short_low_price = cANLStock.GetLow(20, date);
+			float short_low_pricePa = (short_low_price - long_base_price)/long_base_price;
+			float short_high_price = cANLStock.GetHigh(20, date);
+			float short_high_pricePa = (short_high_price - long_base_price)/long_base_price;
+			float short_pianlirate = 0.0f;
+			if(short_high_pricePa-short_low_pricePa != 0)
+				short_pianlirate = (cur_pricePa-short_low_pricePa)/(short_high_pricePa-short_low_pricePa);
+
+			// 计算中期偏离比
+			float mid_low_price = cANLStock.GetLow(60, date);
 			float mid_low_pricePa = (mid_low_price - long_base_price)/long_base_price;
+			float mid_high_price = cANLStock.GetHigh(60, date);
+			float mid_high_pricePa = (mid_high_price - long_base_price)/long_base_price;
+			float mid_pianlirate = 0.0f;
+			if(mid_high_pricePa-mid_low_pricePa != 0)
+				mid_pianlirate = (cur_pricePa-mid_low_pricePa)/(mid_high_pricePa-mid_low_pricePa);
+
+			// 计算长期偏离比
+			float long_low_price = cANLStock.GetLow(250, date);
+			float long_low_pricePa = (long_low_price - long_base_price)/long_base_price;
+			float long_high_price = cANLStock.GetHigh(250, date);
+			float long_high_pricePa = (long_high_price - long_base_price)/long_base_price;
+			float long_pianlirate = 0.0f;
+			if(long_high_pricePa-long_low_pricePa != 0)
+				long_pianlirate = (cur_pricePa-long_low_pricePa)/(long_high_pricePa-long_low_pricePa);
+			
+			logstr = String.format("    %s test[ %.3f %.3f %.3f]\n", cANLPolicyStockCK.stockID, 
+					short_pianlirate,mid_pianlirate,long_pianlirate);
+			outputLog(logstr);
+			
 			// 短期均值参数
-			float short_ma_price = cANLStock.GetMA(15, date);
-			float short_ma_pricePa = (short_ma_price - long_base_price)/long_base_price;
-						
-			cANLPolicyStockCK.c1 = (cur_price-mid_high_price)
-					+(cur_price-mid_low_pricePa)+(cur_price-short_ma_pricePa);
+			cANLPolicyStockCK.c1 = short_pianlirate*(5/10.0f) + mid_pianlirate*(3/10.0f) + long_pianlirate*(2/10.0f);
 			
 			stockCKList.add(cANLPolicyStockCK);
-//			logstr = String.format("    %s test[ %.3f ]\n", cANLPolicyStockCK.stockID, cANLPolicyStockCK.c1);
-//			outputLog(logstr);
+
 		}
 		//分值表排序
 		Collections.sort(stockCKList);
@@ -124,7 +149,7 @@ public class ANLPolicyJZHG extends ANLPolicy {
 		fmt.format("main begin\n");
 		ANLPolicyJZHG cANLPolicyJZHG = new ANLPolicyJZHG();
 		cANLPolicyJZHG.rmlog();
-		cANLPolicyJZHG.run("2016-09-09", "2016-09-09");
+		cANLPolicyJZHG.run("2016-09-19", "2016-09-19");
 		List<ANLStock> cStockObjList = new ArrayList<ANLStock>();
 		fmt.format("main end\n");
 	}
